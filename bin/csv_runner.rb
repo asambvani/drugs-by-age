15e1668a9f20c57861d268e848295ffd48c7e494
asambvani <alexsambvani@gmail.com>
2017-06-21 10:41:24 -0400

    Fied drugs hash

--- bin/csv_runner.rb
+++ bin/csv_runner.rb
@@ -19,29 +19,44 @@ class CSVRunner
     @data = table.map {|row| row.to_hash}
   end
 
-  def get_drug_names(data)
-    drug_names = data[0].keys.map do |key|
-      key.split("-")[0] if key.include?("-")
-    end
-    drug_names.compact.uniq
+  def get_drug_name(string)
+     if string.include?("-use")
+       string.chomp("-use")
+     elsif string.include?("-frequency")
+       string.chomp("-frequency")
+     end
   end
+
+   def get_drugs_array(data)
+     drug_names = data[0].keys.map do |key|
+       get_drug_name(key)
+     end
+     drug_names.compact.uniq
+   end
+
+   def clean_value(value)
+     value == "-" ? 0.0 : value.to_f
+   end
 
   ## NOTE: This function needs to return an array of drug_hashes!
   def parse_drug_data(row)
-    data_array = row.map do |string, num|
-      if string.include?("-") #NOTE: THIS NEEDS TO BE REGEX ON USE/FREQUENCY
-        drug_hash = {}
-        split_string = string.split("-")
-        drug_hash["drug_name"] = split_string[0]
-        drug_hash[split_string[1]] = num
+    drug_hash = {}
+    x = row.each do |drug_key, value|
+      # binding.pry
+      if drug_key.include?("-use") #NOTE: THIS NEEDS TO BE REGEX ON USE/FREQUENCY
+        name = get_drug_name(drug_key)
+        drug_hash[name] = {}
+        drug_hash[name]["use"] = clean_value(value)
+      elsif drug_key.include?("-frequency")
+        name = get_drug_name(drug_key)
+        drug_hash[name]["frequency"] = clean_value(value)
       end
-      drug_hash
     end
-    data_array.compact
+    drug_hash
   end
 
   def enter_drug_data(data)
-    drugs_array = get_drug_names(data)
+    drugs_array = get_drugs_array(data)
     drugs_array.each do |drug|
       Drug.insert_row(self.db, drug)
     end
@@ -51,7 +66,7 @@ class CSVRunner
     enter_drug_data(self.data)
     self.data.each do |row|
       cohort_id = Cohort.insert_row(self.db, row["age"], row["n"])
-      drugs_hashes_array = parse_drug_data(row)
+      drugs_hashes = parse_drug_data(row)
       binding.pry
       drug_id = Drug.find_id_by_name(self.db, drug_hash["drug_name"])
       #CohortsDrugs.insert_row(self.db, drug_hash["usage"], drug_hash["frequency"], cohort_id, drug_id)
