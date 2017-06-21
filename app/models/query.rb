6ea24a9f92aa61bdd9963fb03b023d376f37e6f0
davidtom <davidjtomczyk@gmail.com>
2017-06-21 14:47:30 -0400

    create flexible query to get a drug's usage or frequency for all cohorts

--- app/models/query.rb
+++ app/models/query.rb
@@ -95,4 +95,56 @@ class Query
       self.db.execute(sql)
     end
 
+    def self.drug_names
+      sql = <<-SQL
+      SELECT drugs.name FROM drugs
+      SQL
+      self.db.execute(sql).map {|drug_array| drug_array[0]}
+    end
+
+    def self.cohorts_drugs_columns
+      sql = <<-SQL
+      PRAGMA table_info(cohorts_drugs)
+      SQL
+      self.db.execute(sql).map{|column_info| column_info[1]}
+    end
+
+    def self.cohorts
+      sql = <<-SQL
+      SELECT cohorts.age FROM cohorts
+      SQL
+      self.db.execute(sql).map {|age_array| age_array[0]}
+    end
+
+    def self.valid_drug_name?(drug_name)
+      self.drug_names.include?(drug_name) ? drug_name : false
+    end
+
+    def self.valid_cohorts_drugs_columns?(column)
+      self.cohorts_drugs_columns.include?(column) ? column : false
+    end
+
+    def self.valid_cohort?(cohort)
+      self.cohorts.include?(cohort) ? cohort : false
+    end
+
+    def self.measure_by_drug(drug_name, measure)
+      if !self.valid_drug_name?(drug_name) || !self.valid_cohorts_drugs_columns?(measure)
+        if !self.valid_drug_name?(drug_name)
+          "Error: invalid drug_name: #{drug_name}"
+        elsif !self.valid_cohorts_drugs_columns?(measure)
+          "Error: invalid measure: #{measure}"
+        end
+      else
+        sql = <<-SQL
+        SELECT cohorts.age, cohorts_drugs.#{measure}, drugs.name
+        FROM cohorts JOIN cohorts_drugs JOIN drugs
+        ON cohorts.id = cohorts_drugs.cohort_id AND
+        cohorts_drugs.drug_id = drugs.id
+        WHERE drugs.name = ?;
+        SQL
+        self.db.execute(sql, drug_name)
+      end
+    end
+
 end
